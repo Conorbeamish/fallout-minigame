ccea496eaf46c4bd1c18eb3bda27aaadd868d632
conorbeamish <conorbeamish@hotmail.co.uk>
2020-01-06 15:08:40 +0000

    Add more ACII

--- words.js
+++ words.js
@@ -42,15 +42,15 @@ var hard = [
 ]
 
 var ascii = [
-    "]4V#!R+R3R}@GI^g NO o4P_86f:v)rcC",
-    "uJs>O0elDH MTOp1iDgq M#]';of(0JNRDvxaYJC|",
-    "5>`7DPPFk\JL#,<hPCXgAH>L%87",
-    "NjoAv;LP&@,<H ?9M91k6yt~_r.}q=WrerezBK",
-    "o;9QwqQzQL!$A*H;.?k ?f",
-    "@W~tWxYW\")R^PR<*G {y_# f1T]SUqd DOE=aU",
-    "5nT u|eivhaKV6VLBW}G t%A_P)gv^BZN",
-    "w8%#x-X3=k^_m?P{ LK3e9zrt%}_$7:)4g(WM",
-    "Hz>~5Y):4x6>-6y2T 6w_fXhlung\=4&61u,;r",
-    "f+PBh1]jK&N[cf`DLy s#I+(@0q^_D7![`.1U{k",
+    "]4V#!R+ R3R}@GI^g  NO o4P_86f:v)rcC",
+    "uJs>O0e lDH   MTOp1iDgq M#]';o f(0JNRDv  xaYJC|",
+    "5>`7DP PFk\JL#,<h PCXgAH>L%87",
+    "NjoA  v;LP&@,<H ?9M9 1k6yt~_ r.}q=Wre  rezBK",
+    "o;9Qw qQzQL !$A*  H;.?k ?f",
+    "@W~tWxYW  \")R^PR<*G {y_#  f1T]SUqd DOE=aU",
+    "5nT u|e ivhaKV 6V LBW}G t%A _P)gv^BZN",
+    "w8%#x-X 3=k^_m?P{ LK3e9zr t%}_$7:)4g(WM",
+    "Hz>~5Y): 4x6>-6y2T 6w_f Xhlung\=4&61u,;r",
+    "f+PBh1 ]jK&N[cf `DLy s#I+(@ 0q^_D7![`.1U{k",
 
 ]
