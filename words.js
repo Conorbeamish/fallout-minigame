c8d5554d26535485f10b7194be04ca89af6a4547
conorbeamish <conorbeamish@hotmail.co.uk>
2020-01-06 14:48:59 +0000

    Change ASCII characters

--- words.js
+++ words.js
@@ -42,15 +42,15 @@ var hard = [
 ]
 
 var ascii = [
-    "]4V#!R+R3R}@GI^g NO ;g$B9o<o4P_86f:v)rcC",
+    "]4V#!R+R3R}@GI^g NO o4P_86f:v)rcC",
     "uJs>O0elDH MTOp1iDgq M#]';of(0JNRDvxaYJC|",
-    "5>`7DPPFk\J:33k}:AR ;pL#,<hPCXgAH>L%87",
-    "NjoAv;LM0Q|E*P&@,<H ?9M91k6yt~_r.}q=WzBK",
-    "o;9QwqQzQL!$A*H;.?k ?-F-SsUtsX</--68r6Gf",
+    "5>`7DPPFk\JL#,<hPCXgAH>L%87",
+    "NjoAv;LP&@,<H ?9M91k6yt~_r.}q=WrerezBK",
+    "o;9QwqQzQL!$A*H;.?k ?f",
     "@W~tWxYW\")R^PR<*G {y_# f1T]SUqd DOE=aU",
-    "5nT u|eivhaKV6VLBW}G t%A_P)g0My.QQ%v^BZN",
-    "w8Qz!{%#x-X3=k^_m?P{ LK3e9zrt%}_$7:)4g(WM",
-    "Hz>~5YF=C):4x6>-6y2T 6w_fXhlung\=4&61u,;r",
-    "f+PBh1]jKCT&N[cf`DLy s#I+(@0q^_D7![`.1U{k",
+    "5nT u|eivhaKV6VLBW}G t%A_P)gv^BZN",
+    "w8%#x-X3=k^_m?P{ LK3e9zrt%}_$7:)4g(WM",
+    "Hz>~5Y):4x6>-6y2T 6w_fXhlung\=4&61u,;r",
+    "f+PBh1]jK&N[cf`DLy s#I+(@0q^_D7![`.1U{k",
 
 ]
